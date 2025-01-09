73ffeecdc9cfe6e9b98206755b863bbc90ee612d
TusProtein <maianhtu1111@gmail.com>
2025-01-09 10:54:25 +0700

    Eslint Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0);
-    const left = 'hello';
-    return (
-        <>
-            <h1>Hello</h1>
-        </>
-    );
+  const [count, setCount] = useState(0)
+  const left = 'hello'
+
+  return (
+    <>
+      <a href="" />
+      <h1>Hello</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
