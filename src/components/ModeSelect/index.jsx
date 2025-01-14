0d17a0e3b152c1ebf67a9867edc165946e8d8cc3
TusProtein <maianhtu1111@gmail.com>
2025-01-14 12:21:38 +0700

    Refactor Layout

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,63 @@
+import { FormControl, useColorScheme } from '@mui/material'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import Select from '@mui/material/Select'
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    setMode(event.target.value)
+  }
+
+  const StyledBox = ({ children }) => (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 1
+      }}
+    >
+      {children}
+    </Box>
+  )
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel sx={{ color: 'text.primary' }} id="select-mode-label">
+        Mode
+      </InputLabel>
+      <Select
+        labelId="select-mode-label"
+        id="select-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <StyledBox>
+            <LightModeIcon />
+            Light
+          </StyledBox>
+        </MenuItem>
+        <MenuItem value="dark">
+          <StyledBox>
+            <DarkModeIcon />
+            Dark
+          </StyledBox>
+        </MenuItem>
+        <MenuItem value="system">
+          <StyledBox>
+            <SettingsBrightnessIcon />
+            System
+          </StyledBox>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default ModeSelect
---
+++ src/pages/Boards/BoardBar/index.jsx
@@ -0,0 +1,18 @@
+import Box from '@mui/material/Box'
+import StyledBoxApp from '../../../styles/styledBoxApp'
+
+function BoardBar() {
+  return (
+    <Box
+      sx={(theme) => ({
+        ...StyledBoxApp,
+        height: theme.trello.boardBarHeight,
+        backgroundColor: 'primary.dark'
+      })}
+    >
+      Board Bar
+    </Box>
+  )
+}
+
+export default BoardBar
